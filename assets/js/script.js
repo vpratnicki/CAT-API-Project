c69ad2cbd984bf6d4d2fcf1e923f97a1b0ec7a9e
vpratnicki <vanessa@pratnicki.net>
2022-09-09 23:39:02 -0400

    more css updates and first API added

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+// var catContainerEl = document.querySelector("#cat-container");
+// var getCatsButtonsEl = document.querySelector('#images');
+
+
+ //display random images in a grid 
+var requestUrl= `https://api.thecatapi.com/v1/images/search?limit=6&api_key=live_XVBvacawr5DEpFchrnVEnA6U24e9Dq6sPNr0HgYPz4WBYlSMczHesXIoa76DsSPc`;
+ fetch(requestUrl)
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
+  var imagesData = data;
+  imagesData.map(function(imageData) {
+    
+    var image = document.createElement('img');
+    //use the url from the image object
+    image.src = `${imageData.url}`;
+        
+    var gridCell = document.createElement('div');
+    gridCell.classList.add('col');
+    gridCell.classList.add('col-lg');
+    gridCell.appendChild(image)
+      
+    document.getElementById('grid').appendChild(gridCell);
+    
+    });
+})
+.catch(function (error) {
+    alert('Unable to connect to The Cat API');
+});
+
+ //display random gifs in a grid 
+ var requestUrl= `https://api.thecatapi.com/v1/images/search?limit=6&mime_types=gif&api_key=live_XVBvacawr5DEpFchrnVEnA6U24e9Dq6sPNr0HgYPz4WBYlSMczHesXIoa76DsSPc`;
+ fetch(requestUrl)
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
+  var gifData = data;
+  gifData.map(function(gifData) {
+    
+    var image = document.createElement('img');
+    //use the url from the image object
+    image.src = `${gifData.url}`;
+        
+    var gridCell = document.createElement('div');
+    gridCell.classList.add('col');
+    gridCell.classList.add('col-lg');
+    gridCell.appendChild(image)
+      
+    document.getElementById('grid').appendChild(gridCell);
+    
+    });
+})
+.catch(function (error) {
+    alert('Unable to connect to The Cat API');
+});
