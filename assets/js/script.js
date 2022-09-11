7602976f13184b0e8f8961a7698610557f6e916b
vpratnicki <vanessa@pratnicki.net>
2022-09-11 17:20:07 -0400

    adding event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-// var catContainerEl = document.querySelector("#cat-container");
-// var getCatsButtonsEl = document.querySelector('#images');
+var catContainerEl = document.querySelector("#grid");
+var getMediaButtonEl = document.querySelector("#media");
 
-
+var getCatImages = function () {
  //display random images in a grid 
 var requestUrl= `https://api.thecatapi.com/v1/images/search?limit=6&api_key=live_XVBvacawr5DEpFchrnVEnA6U24e9Dq6sPNr0HgYPz4WBYlSMczHesXIoa76DsSPc`;
  fetch(requestUrl)
@@ -13,7 +13,7 @@ var requestUrl= `https://api.thecatapi.com/v1/images/search?limit=6&api_key=live
   imagesData.map(function(imageData) {
     
     var image = document.createElement('img');
-    //use the url from the image object
+    //use the url to display the image
     image.src = `${imageData.url}`;
         
     var gridCell = document.createElement('div');
@@ -22,13 +22,14 @@ var requestUrl= `https://api.thecatapi.com/v1/images/search?limit=6&api_key=live
     gridCell.appendChild(image)
       
     document.getElementById('grid').appendChild(gridCell);
-    
     });
 })
 .catch(function (error) {
     alert('Unable to connect to The Cat API');
 });
+};
 
+var getCatGifs = function () {
  //display random gifs in a grid 
  var requestUrl= `https://api.thecatapi.com/v1/images/search?limit=6&mime_types=gif&api_key=live_XVBvacawr5DEpFchrnVEnA6U24e9Dq6sPNr0HgYPz4WBYlSMczHesXIoa76DsSPc`;
  fetch(requestUrl)
@@ -55,3 +56,24 @@ var requestUrl= `https://api.thecatapi.com/v1/images/search?limit=6&api_key=live
 .catch(function (error) {
     alert('Unable to connect to The Cat API');
 });
+};
+
+var buttonClickHandler = function (event) {
+  var mediaType = event.target.getAttribute("data-type");
+  console.log(mediaType);
+
+  if ("data-type" === "img") {
+    getCatImages(mediaType);
+  } else if ("data-type" === "img") {
+    getCatGifs(mediaType);
+  } else {
+    alert('Unable to find cats! They are napping.');
+  }
+
+  // clear old content
+  catContainerEl.textContent = "";
+
+}
+
+getMediaButtonEl.addEventListener("click", buttonClickHandler);
+// getGifsButtonEl.addEventListener("click", getCatGifs);
